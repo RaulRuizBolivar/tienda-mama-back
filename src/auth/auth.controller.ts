0516c75ada553b31b4f4cac1e8f5e4d9c7ecaa0e
Raul <raul.ruiz@wyred.biz>
2024-03-16 14:59:24 +0100

    Added custom decorator isAdmin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { isAdmin } from './decorators/isAdmin.decorator';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
-import { IsAdminGuard } from './guard/isAdmin.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -19,7 +19,7 @@ export class AuthController {
   }
 
   @Get('prueba')
-  @UseGuards(IsAdminGuard)
+  @isAdmin()
   prueba() {
     return 'prueba';
   }
