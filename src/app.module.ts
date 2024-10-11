95595c5bd908830b98db1af756ade131b83590a1
Raul <raul.ruiz.bolivar@gmail.com>
2024-10-11 18:55:35 +0200

    get data to connect to database from .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'database/data-source';
 import { AuthModule } from './auth/auth.module';
 import { CampaignsModule } from './campaigns/campaigns.module';
-import { dataSourceOptions } from './database/data-source';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(dataSourceOptions),
+    TypeOrmModule.forRootAsync(dataSourceOptions),
     UsersModule,
     AuthModule,
     ProductsModule,
---
+++ src/database/data-source.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const dataSourceOptions: TypeOrmModuleAsyncOptions = {
+  useFactory: () => ({
+    type: 'mysql',
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    entities: ['/src/**/*.entity.ts'],
+    migrations: ['/src/database/migrations/*.ts'],
+  }),
+};
